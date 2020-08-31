624972a74f44a548bef1e6a8da7d993c694b2f55
yuxingguang <yuxiaoyubaba@163.com>
2020-08-31 10:35:39 +0800

    sfd

---
+++ 数据结构与算法/排序/bubble.js
@@ -0,0 +1,9 @@
+const {nums} = require('./data.js')
+
+function bubble_sort(nums){
+    for(let num in nums){
+        console.log(num)
+    }
+}
+
+bubble_sort(nums)
---
+++ 数据结构与算法/排序/data.js
@@ -0,0 +1,4 @@
+const nums = [2,1,7,9,5,8];
+module.exports= {
+    nums,
+}
